3257e79a993c3315220c02c27dabb91c7deb3609
Gredler <an.gredler@hotmail.com>
2017-12-09 11:42:45 +0100

    03/10 - Spring Profiles

--- src/main/java/com/example/didemo/services/PrimarySpanishGreetingService.java
+++ src/main/java/com/example/didemo/services/PrimarySpanishGreetingService.java
@@ -4,13 +4,12 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-//@Primary means that if there is more than one Bean that qualifies, it will use this one.
 @Service
-@Profile("en")
+@Profile("es")
 @Primary
-public class PrimaryGreetingService implements GreetingService {
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Servicio de Saludo Primario";
     }
 }
