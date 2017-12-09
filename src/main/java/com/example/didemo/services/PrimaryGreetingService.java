cb7138ce010445826c236409cfb2524aa2e79ad8
Gredler <an.gredler@hotmail.com>
2017-12-09 16:59:28 +0100

    05/07 - Using Factory Beans

--- src/main/java/com/example/didemo/services/PrimaryGreetingService.java
+++ src/main/java/com/example/didemo/services/PrimaryGreetingService.java
@@ -1,17 +1,17 @@
 package com.example.didemo.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
 //@Primary means that if there is more than one Bean that qualifies, it will use this one.
-@Service
 // The default-Profile is only active, when other profiles are not active!
-@Profile({"en", "default"})
-@Primary
 public class PrimaryGreetingService implements GreetingService {
+
+    private GreetingRepository greetingRepository;
+
+    public PrimaryGreetingService(GreetingRepository greetingRepository) {
+        this.greetingRepository = greetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return greetingRepository.getEnglishGreeting();
     }
 }
